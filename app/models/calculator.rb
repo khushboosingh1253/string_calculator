bf4319e9652c3e13737f761bfa1a045de4b7bc05
root <root@DESKTOP-1KJT96A>
2025-05-19 18:11:06 +0400

    Test Negative numbers

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -3,9 +3,16 @@ class Calculator
     return 0 if numbers.empty?
     if numbers.start_with?("//")
       delimiter, numbers = numbers[2..].split("\n", 2)
-      numbers.split(Regexp.escape(delimiter)).map(&:to_i).sum
+      numbers = numbers.split(Regexp.escape(delimiter))
     else
-      numbers.gsub("\n", ",").split(",").map(&:to_i).sum
+      numbers = numbers.gsub("\n", ",").split(",")
     end
+
+    numbers = numbers.map(&:to_i)
+    negatives = numbers.select(&:negative?)
+  
+    raise "negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
+  
+    numbers.sum
   end
 end
--- spec/models/calculator_spec.rb
+++ spec/models/calculator_spec.rb
@@ -25,5 +25,10 @@ RSpec.describe Calculator, type: :model do
     it 'supports custom delimiters' do
       expect(Calculator.add("//;\n1;2")).to eq(3)
     end
+    
+    it 'raises an error for negative numbers' do
+      expect { Calculator.add("1,-2,3,-4") }
+        .to raise_error("negative numbers not allowed: -2, -4")
+    end
   end
 end
