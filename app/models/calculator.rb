6f60e35617a09c5602243f44e4e39b5950aff6d9
root <root@DESKTOP-1KJT96A>
2025-05-19 11:27:21 +0400

    Support Custom Delimiters

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -1,6 +1,11 @@
 class Calculator
   def self.add(numbers)
     return 0 if numbers.empty?
-    numbers.gsub("\n", ",").split(",").map(&:to_i).sum
+    if numbers.start_with?("//")
+      delimiter, numbers = numbers[2..].split("\n", 2)
+      numbers.split(Regexp.escape(delimiter)).map(&:to_i).sum
+    else
+      numbers.gsub("\n", ",").split(",").map(&:to_i).sum
+    end
   end
 end
